f7c8c66a85dc952a631e539afa81f88daf8e20a3
akbarjon2000 <1543431a@gmail.com>
2021-10-18 22:09:25 +0900

    learning git codes

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Tst } from "./tst"
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div>
+    <Tst />
+  </div>,
   document.getElementById('root')
 );
 
---
+++ src/tst.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Tst() {
+    return (
+        <div>
+            <h2>This is test file</h2>
+        </div>
+    )
+}
+
+export { Tst }
